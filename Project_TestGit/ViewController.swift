b61248d7fcb1c8a79925b713c3f2589a3774a081
Humanken <3hotpeper@gmail.com>
2023-03-27 12:53:52 +0800

    add SplitViewController

--- Project_TestGit/ViewController.swift
+++ Project_TestGit/ViewController.swift
@@ -17,3 +17,45 @@ class ViewController: UIViewController {
 
 }
 
+class splitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let containVC = UIViewController()
+        containVC.view.backgroundColor = .systemBlue
+        
+        self.viewControllers = [
+            UINavigationController(rootViewController: MenuController(style: .plain)),
+            UINavigationController(rootViewController: containVC)
+        ]
+    }
+
+}
+
+class MenuController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+    }
+    
+    private var cellTexts = ["桌號", "經典涮肉", "風味涮肉", "素食區", "附餐區"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return cellTexts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        print("tableView cell: \(cell)")
+        cell.textLabel?.text = cellTexts[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+
+}
+
